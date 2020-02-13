8a622fd8ca0f515fd3ee40d49de1cdc4e2349a56
Jonathan Mongeau <jonthemango@gmail.com>
2020-02-12 19:32:18 -0500

    Added init_spark as util and added test for dump

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+from utils import dump, init_spark
+
+def test_dump():
+    spark = init_spark()
+    sc = spark.sparkContext
+    data = [1, 2, 3, 4, 5]
+    distData = sc.parallelize(data)
+
+    fn = "test_dump"
+    dump(distData,fn)
+    with open(fn, 'r') as f:
+        assert len(f.readlines()) == len(data)
+    
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,19 @@
+from pyspark.rdd import RDD
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
 
-def dump(rdd):
-    from pyspark.sql import DataFrame
+def dump(rdd, fn="out"):
     if isinstance(rdd, DataFrame):
         rdd = rdd.rdd
-    with open('out', 'w+') as f:
+    with open(fn, 'w+') as f:
         text = rdd.map(lambda row: str(row) + "\n").reduce(lambda row1,row2: row1+ row2)
         f.write(text)
     return True
 
+def init_spark():
+    spark = SparkSession \
+        .builder \
+        .appName("Python Spark SQL basic example") \
+        .config("spark.some.config.option", "some-value") \
+        .getOrCreate()
+    return spark
