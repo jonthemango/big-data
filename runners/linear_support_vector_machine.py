8154ddc4d7772961d2f2754b5282f4d47dd0b2ac
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-03-19 18:52:26 -0400

    Add linear SVM training algorithm
    Co-authored-by: feras.younis@outlook.com <feras.younis@outlook.com>

---
+++ runners/linear_support_vector_machine.py
@@ -0,0 +1,44 @@
+from preprocessing import step2_feature_engineering as feature_eng
+
+from pyspark.rdd import RDD
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
+import json
+import copy as cp
+import utils
+
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.ml.classification import LinearSVC
+
+
+def driver():
+    data_df, features = feature_eng.preprocess_features()
+    data_df.cache()
+    # Split the data into training and test sets (30% held out for testing)
+    (trainingData, testData) = data_df.randomSplit([0.7, 0.3])
+
+    # Assemble all features in a vector using Vector Assembler
+    # map it to new column named features
+    vector_assembler = VectorAssembler(
+        inputCols=features, outputCol="features")
+
+    lsvc = LinearSVC(labelCol='TARGET', maxIter=10, regParam=0.1)
+
+    # Chain vector_assembler and lsvc in a Pipeline
+    pipeline = Pipeline(stages=[vector_assembler, lsvc])
+
+    # Fit the model
+    lsvcModel = pipeline.fit(trainingData)
+
+    predictions = lsvcModel.transform(testData)
+    predictions.select('TARGET', 'rawPrediction', 'prediction').show(150)
+
+    evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction",
+                                              labelCol="TARGET", metricName='areaUnderROC')
+
+    areaUnderRoc = evaluator.evaluate(predictions)
+    print(f"Area Under ROC = {areaUnderRoc}")
+
+driver()
