f2025aacc8f077ca6bed394a6de7d0a6c1b4098a
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-03-19 17:34:40 -0400

    Add 3 training algorithms and evaluate their ROC AUC
    Co-authored-by: Jonathan Mongeau <jonthemango@gmail.com>
    Co-authored-by: feras.younis@outlook.com <feras.younis@outlook.com>

---
+++ runners/naive_bayes.py
@@ -0,0 +1,48 @@
+from preprocessing import step2_feature_engineering as feature_eng
+
+from pyspark.rdd import RDD
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
+import json
+import copy as cp
+import utils
+
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import NaiveBayes
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+
+def driver():
+    data_df, features = feature_eng.preprocess_features()
+    data_df.cache()
+    # Split the data into training and test sets (30% held out for testing)
+    (trainingData, testData) = data_df.randomSplit([0.7, 0.3])
+'''
+OCUPATION_TYPE: 1 | 5 | 9
+1, 1,0
+5, 1,0
+9, 1,0
+'''
+    # create the trainer and set its parameters
+    nb = NaiveBayes(labelCol='TARGET', featuresCol='OCCUPATION_TYPE',
+                    smoothing=1.0, modelType="multinomial")
+
+    # Chain vector_assembler and forest in a Pipeline
+
+    # Train model.  This also runs the indexers.
+    model = nb.fit(trainingData)
+
+    # Make predictions.
+    predictions = model.transform(testData)
+    predictions.select('TARGET', 'rawPrediction', 'prediction','probability').show(20)
+
+    # Select (prediction, true label) and compute test error
+    evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction",
+                                              labelCol="TARGET", metricName='areaUnderROC')
+
+    areaUnderRoc = evaluator.evaluate(predictions)
+    print(f"Area Under ROC = {areaUnderRoc}")
+
+
+driver()
---
+++ runners/random_forest.py
@@ -0,0 +1,51 @@
+from preprocessing import step2_feature_engineering as feature_eng
+
+from pyspark.rdd import RDD
+from pyspark.sql import DataFrame
+from pyspark.sql import SparkSession
+import json
+import copy as cp
+import utils
+
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+
+def driver():
+
+    # Pre-process features
+    data_df, features = feature_eng.preprocess_features()
+
+    # Split the data into training and test sets (30% held out for testing)
+    (trainingData, testData) = data_df.randomSplit([0.7, 0.3])
+
+    # Assemble all features in a vector using Vector Assembler
+    # map it to new column named features
+    vector_assembler = VectorAssembler(
+        inputCols=features, outputCol="features")
+
+    # Train a RandomForest model.
+    rf = RandomForestClassifier(
+        labelCol="TARGET", featuresCol="features", numTrees=1)
+
+    # Chain vector_assembler and forest in a Pipeline
+    pipeline = Pipeline(stages=[vector_assembler, rf])
+
+    # Train model.  This also runs the indexers.
+    model = pipeline.fit(trainingData)
+
+    # Make predictions.
+    predictions = model.transform(testData)
+    predictions.select('TARGET', 'rawPrediction').show(20)
+
+    # Select (prediction, true label) and compute test error
+    evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction",
+                                              labelCol="TARGET", metricName='areaUnderROC')
+
+    areaUnderRoc = evaluator.evaluate(predictions)
+    print(f"Area Under ROC = {areaUnderRoc}")
+
+
+driver()
