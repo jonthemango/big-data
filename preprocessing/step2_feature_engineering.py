043b94f1e858a44f48865232d471d24e135a0ef3
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-03-23 00:42:48 -0400

    Include min, max, avg of columns in bureau.csv as features
    10 min training random forest yields 0.67 ROC AUC
    
    Co-authored-by: Jonathan Mongeau <jonthemango@gmail.com>

--- preprocessing/step2_feature_engineering.py
+++ preprocessing/step2_feature_engineering.py
@@ -1,6 +1,7 @@
 from pyspark.rdd import RDD
 from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
 import json
 import copy as cp
 import utils
@@ -15,7 +16,7 @@ import pyspark.sql.types as sparkTypes
 root = utils.get_project_root_dir()
 
 
-def cast_column_to_type(df, column_name, sql_type=sparkTypes.IntegerType):
+def cast_column_to_type(df, column_name, sql_type=sparkTypes.FloatType):
     '''
     Transforms a df at a particular column_name by casting all values to the specified type, default is IntegerType
     '''
@@ -35,7 +36,8 @@ def encode_using_one_hot(df, column_name):
     df = StringIndexer(inputCol=column_name, outputCol=indexed_name,
                        handleInvalid="skip").fit(df).transform(df)
 
-    encoder = OneHotEncoderEstimator(inputCols=[indexed_name], outputCols=[vectored_name])
+    encoder = OneHotEncoderEstimator(
+        inputCols=[indexed_name], outputCols=[vectored_name])
     model = encoder.fit(df)
     df = model.transform(df)
     df = df.drop(indexed_name)
@@ -56,8 +58,19 @@ def encode_using_indexer(df, column_name):
     df = df.withColumnRenamed(indexed_name, column_name)
     return df
 
-def funcname(self, parameter_list):
-    pass
+
+def encode_using_stats(df, column_names, sql_type=sparkTypes.FloatType):
+    '''Returns an aggregated df of the  min, max, and mean of a column'''
+    aggregations = []
+    for column_name in column_names:
+        df = cast_column_to_type(df, column_name, sql_type)
+        aggregations.append(F.avg(column_name))
+        aggregations.append(F.min(column_name))
+        aggregations.append(F.max(column_name))
+
+    agg_df = df.groupBy("SK_ID_CURR").agg(*aggregations) #.agg(aggregations)
+    return agg_df, agg_df.columns
+
 
 def preprocess_features():
     application_filename = f'{root}data/application_train.csv'
@@ -66,33 +79,39 @@ def preprocess_features():
     spark = utils.init_spark()
     data_df = spark.read.csv(application_filename, header=True)
 
-    
+    # Count of Applicant's Previous Loans
     previous_loans_df = spark.read.csv(bureau_filename, header=True)
-    previous_loan_count = previous_loans_df.groupBy('SK_ID_CURR').count().na.fill(0,"count")
-    data_df = data_df.join(previous_loan_count, on="SK_ID_CURR")
-    data_df.select('SK_ID_CURR', 'count').show(10)
+    previous_loan_count = previous_loans_df.groupBy('SK_ID_CURR').count().na.fill(0, "count")  # [SK_ID_CURR, count]
+    data_df = data_df.join(previous_loan_count,on="SK_ID_CURR")  # join on SK_ID_CURR
 
-    
+    # Numerical Data
+    agg_df, _ = encode_using_stats(previous_loans_df, ['DAYS_CREDIT', 'CREDIT_DAY_OVERDUE'])
+    data_df = data_df.join(agg_df, on="SK_ID_CURR")
+    agg_df.show(10)
 
     # List of Features
     features = [
         'FLAG_OWN_CAR',
         'CODE_GENDER',
-        'AMT_GOODS_PRICE', 
-        'DAYS_EMPLOYED', 
-        'DAYS_BIRTH', 
-        'FLAG_DOCUMENT_2', 
-        'FLAG_DOCUMENT_3', 
+        'AMT_GOODS_PRICE',
+        'DAYS_EMPLOYED',
+        'DAYS_BIRTH',
+        'FLAG_DOCUMENT_2',
+        'FLAG_DOCUMENT_3',
         'FLAG_DOCUMENT_4',
-        'AMT_CREDIT', 
+        'AMT_CREDIT',
         'FLAG_OWN_REALTY',
-        'FLAG_MOBIL', 
-        'NAME_FAMILY_STATUS', 
-        'NAME_TYPE_SUITE', 
-        'NAME_EDUCATION_TYPE', 
-        'NAME_CONTRACT_TYPE', 
+        'FLAG_MOBIL',
+        'NAME_FAMILY_STATUS',
+        'NAME_TYPE_SUITE',
+        'NAME_EDUCATION_TYPE',
+        'NAME_CONTRACT_TYPE',
         'NAME_INCOME_TYPE',
-        'count'
+        'count',
+        'avg(CREDIT_DAY_OVERDUE)',
+        'min(DAYS_CREDIT)',
+        'max(DAYS_CREDIT)',
+        'avg(DAYS_CREDIT)'
     ]
 
     # Feature Encoding
@@ -103,6 +122,10 @@ def preprocess_features():
     data_df = cast_column_to_type(data_df, 'AMT_GOODS_PRICE', sparkTypes.FloatType)
     data_df = cast_column_to_type(data_df, 'DAYS_EMPLOYED', sparkTypes.FloatType)
     data_df = cast_column_to_type(data_df, 'DAYS_BIRTH', sparkTypes.FloatType)
+    data_df = cast_column_to_type(data_df, 'avg(CREDIT_DAY_OVERDUE)', sparkTypes.FloatType)
+    data_df = cast_column_to_type(data_df, 'min(DAYS_CREDIT)', sparkTypes.FloatType)
+    data_df = cast_column_to_type(data_df, 'max(DAYS_CREDIT)', sparkTypes.FloatType)
+    data_df = cast_column_to_type(data_df, 'avg(DAYS_CREDIT)', sparkTypes.FloatType)
 
     # Indexer
     data_df = encode_using_indexer(data_df, 'FLAG_OWN_CAR')
