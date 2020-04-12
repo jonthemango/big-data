166244de44f3a1ffd805c111bd44989cc54f9804
Jonathan Mongeau <jonthemango@gmail.com>
2020-04-12 16:59:12 -0400

    Added sample

--- data/extract_sample.py
+++ data/extract_sample.py
@@ -1,13 +1,18 @@
 import os
 
 '''
-Run this file to take the first 20 lines of each CSV file in this
+Run this file to take the first 300 lines of each CSV file in this
 directory and output them to /samples/sample_*.csv
 '''
 for filename in os.listdir("./"):
     if filename.endswith(".csv"):
         with open(filename) as myfile:
-            head = [next(myfile) for x in range(100)]
-            with open('sample/sample_'+filename, 'w+') as newfile:
-                for line in head:
-                    newfile.write(line)
+            try:
+                head = [next(myfile) for x in range(300)]
+                with open('sample/sample_'+filename, 'w+') as newfile:
+                    for line in head:
+                        newfile.write(line)
+            except:
+                with open('sample/sample_'+filename, 'w+') as newfile:
+                    for line in head:
+                        newfile.write(line)
